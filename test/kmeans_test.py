fa6d9eda956bf62b20d437c20211b04d41063f12
darcy <darcy.yang@hotmail.com>
2016-08-11 16:12:45 +0800

    first commit

---
+++ test/kmeans_test.py
@@ -0,0 +1,33 @@
+#################################################  
+# kmeans: k-means cluster  
+# Author : zouxy  
+# Date   : 2013-12-25  
+# HomePage : http://blog.csdn.net/zouxy09  
+# Email  : zouxy09@qq.com  
+#################################################  
+  
+from numpy import *  
+import time  
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.append("..")
+from algrithem import kmeans
+  
+# # step 1: load data  
+print "step 1: load data..."  
+dataSet = []  
+fileIn = open('/Users/darcy/pywork/python-machine-learning/data/kmeans_dataSet.txt')  
+for line in fileIn.readlines():  
+    lineArr = line.strip().split('\t')  
+    dataSet.append([float(lineArr[0]), float(lineArr[1])])  
+  
+# # step 2: clustering...  
+print "step 2: clustering..."  
+dataSet = mat(dataSet)  
+k = 4  
+centroids, clusterAssment = kmeans.kmeans(dataSet, k)  
+  
+# # step 3: show the result  
+print "step 3: show the result..."  
+kmeans.showCluster(dataSet, k, centroids, clusterAssment) 
